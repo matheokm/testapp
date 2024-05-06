f420de168205ccb129db38687fb563ed3dac9813
Kevin Matheo Paspuel E <Matheo.Paspuel@sertecpet.net>
2024-05-06 17:13:29 -0500

    Carga de Index redireccionando a Mis solicitudes (maquetado)

--- lib/screens/indexPage.dart
+++ lib/screens/indexPage.dart
@@ -6,6 +6,9 @@ class IndexPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.popAndPushNamed(context, 'fuel_request');
+    });
     return Scaffold(
       appBar: AppBar(title: const Text('Despacho de combustible')),
       drawer: const MenuWidget(),
