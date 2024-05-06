ab719c8f57ada35b4df3790f70d113530ade5c09
Kevin Matheo Paspuel E <Matheo.Paspuel@sertecpet.net>
2024-05-06 13:40:48 -0500

    Se refactoriza el archivo main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:testapp/screens/editar.dart';
+import 'package:testapp/screens/dataSyncPage.dart';
+import 'package:testapp/screens/profilePage.dart';
+import 'package:testapp/screens/transferJournalsNewPage.dart';
+import 'package:testapp/screens/fuelRequestNewPage.dart';
+import 'package:testapp/screens/fuelRequestPage.dart';
 import 'package:testapp/screens/indexPage.dart';
-import 'package:testapp/screens/listado.dart';
 import 'package:testapp/screens/loginPage.dart';
+import 'package:testapp/screens/pendingFuelRequestPage.dart';
 import 'package:testapp/screens/startPage.dart';
-import 'package:testapp/screens/test/customize_drawer_screen.dart';
+import 'package:testapp/screens/transferJournalsPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +17,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  //This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,28 +25,18 @@ class MyApp extends StatelessWidget {
           fontFamily: ('inter'),
           useMaterial3: true,
         ),
-        initialRoute: '/', // Ruta inicial
+        initialRoute: '/',
         routes: {
           '/': (context) => const StartPage(),
           'login': (context) => const LoginPage(),
           'index': (context) => const IndexPage(),
-          'listado': (context) => const Lista(),
-          'editar': (context) => Editar(), // Ruta para la página de inicio
-          // Agrega más rutas según sea necesario para otras páginas
-          // Por ejemplo:
-          // '/other': (context) => OtherPage(),
+          'fuel_request': (context) => const FuelRequestPage(),
+          'pending_fuel_request': (context) => const PendingFuelRequestPage(),
+          'fuel_request_new': (context) => const FuelRequestNewPage(),
+          'transfer_journals': (context) => const TransferJournalsPage(),
+          'transfer_journals_new': (context) => const TransferJournalsNewPage(),
+          'data_sync': (context) => const DataSyncPage(),
+          'profile': (context) => const ProfilePage(),
         });
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //     debugShowCheckedModeBanner: false,
-  //     title: 'Flutter Demo',
-  //     theme: ThemeData(
-  //       primarySwatch: Colors.blue,
-  //     ),
-  //     home: const StartPage(),
-  //   );
-  // }
 }
