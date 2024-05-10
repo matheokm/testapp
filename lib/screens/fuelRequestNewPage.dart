58545b8e722bb286bdcd4f2e287160724fed5afa
Kevin Matheo Paspuel E <Matheo.Paspuel@sertecpet.net>
2024-05-10 17:01:58 -0500

    Se agrega diseño primario del formulario

--- lib/screens/fuelRequestNewPage.dart
+++ lib/screens/fuelRequestNewPage.dart
@@ -6,9 +6,110 @@ class FuelRequestNewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> localidades = [
+      'Quito',
+      'Guayaquil',
+      'Cuenca',
+      'Loja',
+      'Ambato'
+    ];
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Solicitud de combustible'),
+        title: const Align(
+            alignment: Alignment.bottomLeft,
+            child: Text('Solicitud de combustible')
+        ),
+        backgroundColor: Color.fromARGB(255, 109, 108, 108),
+        foregroundColor: Color.fromARGB(255, 223, 221, 221),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              DropdownButtonFormField<String>(
+                hint: const Text('Tipo de combustible'),
+                items:
+                    <String>['Gasolina', 'Diesel', 'Gas'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (_) {},
+              ),
+              TextFormField(
+                decoration: const InputDecoration(hintText: 'Cantidad (gal)'),
+              ),
+              Autocomplete<String>(
+                optionsBuilder: (TextEditingValue textEditingValue) {
+                  if (textEditingValue.text == '') {
+                    return const Iterable<String>.empty();
+                  }
+                  return localidades.where((String option) {
+                    return option.contains(textEditingValue.text.toLowerCase());
+                  });
+                },
+                onSelected: (String selection) {
+                  print('Has seleccionado: $selection');
+                },
+                fieldViewBuilder: (BuildContext context,
+                    TextEditingController textEditingController,
+                    FocusNode focusNode,
+                    VoidCallback onFieldSubmitted) {
+                  return TextFormField(
+                    controller: textEditingController,
+                    focusNode: focusNode,
+                    onFieldSubmitted: (String value) {
+                      onFieldSubmitted();
+                    },
+                    decoration: const InputDecoration(
+                      hintText: 'Localidad origen',
+                    ),
+                  );
+                },
+              ),
+              Autocomplete<String>(
+                optionsBuilder: (TextEditingValue textEditingValue) {
+                  if (textEditingValue.text == '') {
+                    return const Iterable<String>.empty();
+                  }
+                  return localidades.where((String option) {
+                    return option.contains(textEditingValue.text.toLowerCase());
+                  });
+                },
+                onSelected: (String selection) {
+                  print('Has seleccionado: $selection');
+                },
+                fieldViewBuilder: (BuildContext context,
+                    TextEditingController textEditingController,
+                    FocusNode focusNode,
+                    VoidCallback onFieldSubmitted) {
+                  return TextFormField(
+                    controller: textEditingController,
+                    focusNode: focusNode,
+                    onFieldSubmitted: (String value) {
+                      onFieldSubmitted();
+                    },
+                    decoration: const InputDecoration(
+                      hintText: 'Localidad destino',
+                    ),
+                  );
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(hintText: 'Descripción'),
+                maxLines: 2,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Guardar la información
+                },
+                child: const Text('Guardar'),
+              ),
+            ],
+          ),
+        ),
       ),
       drawer: const MenuWidget(),
     );
