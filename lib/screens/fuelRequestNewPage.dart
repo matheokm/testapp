d1d6b4bc9b4d5c628911d7b94a7edd075a7b058f
Kevin Matheo Paspuel E <Matheo.Paspuel@sertecpet.net>
2024-05-17 16:42:47 -0500

    Se modifica diseño UI

--- lib/screens/fuelRequestNewPage.dart
+++ lib/screens/fuelRequestNewPage.dart
@@ -6,31 +6,138 @@ class FuelRequestNewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> localidades = [
-      'Quito',
-      'Guayaquil',
-      'Cuenca',
-      'Loja',
-      'Ambato'
+    final List<String> equipos = [
+      'cbmtu',
+      'cbmtu-01',
+      'cbmtu-11',
+      'cbmtu-21',
+      'cbmtu-31'
     ];
+    final List<String> centroCostos = [
+      '83104 - combustible',
+      '102104 - combustible',
+      '21104 - combustible'
+    ];
+    final List<String> tipoCombustible = [
+      'Diesel filtrado',
+      'Diesel 2 petrolero',
+      'Diesel premium'
+    ];
+    final List<String> tipoDespacho = [
+      'Gasolinera',
+      'Tanque estacionario',
+      'Tanquero',
+      'Tanque almacenamiento en campo'
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Align(
             alignment: Alignment.bottomLeft,
-            child: Text('Solicitud de combustible')
-        ),
-        backgroundColor: Color.fromARGB(255, 109, 108, 108),
-        foregroundColor: Color.fromARGB(255, 223, 221, 221),
+            child: Text('Solicitud de combustible')),
+        backgroundColor: const Color.fromARGB(255, 109, 108, 108),
+        foregroundColor: const Color.fromARGB(255, 223, 221, 221),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Form(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
+              Autocomplete<String>(
+                optionsBuilder: (TextEditingValue textEditingValue) {
+                  if (textEditingValue.text == '') {
+                    return const Iterable<String>.empty();
+                  }
+                  return equipos.where((String option) {
+                    return option.contains(textEditingValue.text.toLowerCase());
+                  });
+                },
+                onSelected: (String selection) {
+                  print('Has seleccionado: $selection');
+                },
+                fieldViewBuilder: (BuildContext context,
+                    TextEditingController textEditingController,
+                    FocusNode focusNode,
+                    VoidCallback onFieldSubmitted) {
+                  return TextFormField(
+                    controller: textEditingController,
+                    focusNode: focusNode,
+                    onFieldSubmitted: (String value) {
+                      onFieldSubmitted();
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text(
+                        'Equipos',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 109, 108, 108),
+                        ),
+                      ),
+                      suffixIcon: Icon(Icons.qr_code_scanner),
+                    ),
+                  );
+                },
+              ),
+              Autocomplete<String>(
+                optionsBuilder: (TextEditingValue textEditingValue) {
+                  if (textEditingValue.text == '') {
+                    return const Iterable<String>.empty();
+                  }
+                  return centroCostos.where((String option) {
+                    return option.contains(textEditingValue.text.toLowerCase());
+                  });
+                },
+                onSelected: (String selection) {
+                  print('Has seleccionado: $selection');
+                },
+                fieldViewBuilder: (BuildContext context,
+                    TextEditingController textEditingController,
+                    FocusNode focusNode,
+                    VoidCallback onFieldSubmitted) {
+                  return TextFormField(
+                    controller: textEditingController,
+                    focusNode: focusNode,
+                    onFieldSubmitted: (String value) {
+                      onFieldSubmitted();
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text(
+                        'Centro de costos',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 109, 108, 108),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    label: Text(
+                      'Cantidad solicitada (gal)',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 109, 108, 108),
+                      ),
+                    )),
+              ),
               DropdownButtonFormField<String>(
-                hint: const Text('Tipo de combustible'),
-                items:
-                    <String>['Gasolina', 'Diesel', 'Gas'].map((String value) {
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text(
+                    'Tipo de combustible',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 109, 108, 108),
+                    ),
+                  ),
+                ),
+                items: tipoCombustible.map((String value) {
                   return DropdownMenuItem<String>(
                     value: value,
                     child: Text(value),
@@ -38,70 +145,45 @@ class FuelRequestNewPage extends StatelessWidget {
                 }).toList(),
                 onChanged: (_) {},
               ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Cantidad (gal)'),
-              ),
-              Autocomplete<String>(
-                optionsBuilder: (TextEditingValue textEditingValue) {
-                  if (textEditingValue.text == '') {
-                    return const Iterable<String>.empty();
-                  }
-                  return localidades.where((String option) {
-                    return option.contains(textEditingValue.text.toLowerCase());
-                  });
-                },
-                onSelected: (String selection) {
-                  print('Has seleccionado: $selection');
-                },
-                fieldViewBuilder: (BuildContext context,
-                    TextEditingController textEditingController,
-                    FocusNode focusNode,
-                    VoidCallback onFieldSubmitted) {
-                  return TextFormField(
-                    controller: textEditingController,
-                    focusNode: focusNode,
-                    onFieldSubmitted: (String value) {
-                      onFieldSubmitted();
-                    },
-                    decoration: const InputDecoration(
-                      hintText: 'Localidad origen',
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text(
+                    'Tipo de despacho',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 109, 108, 108),
                     ),
+                  ),
+                ),
+                items: tipoDespacho.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
                   );
-                },
+                }).toList(),
+                onChanged: (_) {},
               ),
-              Autocomplete<String>(
-                optionsBuilder: (TextEditingValue textEditingValue) {
-                  if (textEditingValue.text == '') {
-                    return const Iterable<String>.empty();
-                  }
-                  return localidades.where((String option) {
-                    return option.contains(textEditingValue.text.toLowerCase());
-                  });
-                },
-                onSelected: (String selection) {
-                  print('Has seleccionado: $selection');
-                },
-                fieldViewBuilder: (BuildContext context,
-                    TextEditingController textEditingController,
-                    FocusNode focusNode,
-                    VoidCallback onFieldSubmitted) {
-                  return TextFormField(
-                    controller: textEditingController,
-                    focusNode: focusNode,
-                    onFieldSubmitted: (String value) {
-                      onFieldSubmitted();
-                    },
-                    decoration: const InputDecoration(
-                      hintText: 'Localidad destino',
-                    ),
-                  );
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Descripción'),
-                maxLines: 2,
+              const TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    label: Text(
+                      'Locación',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 109, 108, 108),
+                      ),
+                    )),
               ),
               ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 109, 108, 108),
+                  foregroundColor: const Color.fromARGB(255, 223, 221, 221),
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(5.0), // 5% del tamaño del botón
+                  ),
+                ),
                 onPressed: () {
                   // Guardar la información
                 },
